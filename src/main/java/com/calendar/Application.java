99aeddf4c9c42181813059bfaf9ba11858f9d10e
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2020-01-08 08:23:24 +0200

    Initial commit

---
+++ src/main/java/com/calendar/Application.java
@@ -0,0 +1,16 @@
+package com.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
